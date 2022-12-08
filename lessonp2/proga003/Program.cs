aa7e069ae1a3ade9de16aafafa88d8d980b389c9
Polenok Sergey <sergeipolenok7@icloud.com>
2022-12-08 15:35:11 +0700

    Ищем нужный элемент массива

---
+++ lessonp2/proga003/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {10,22,33,41,15,6,17,84};
+
+int n = array.Length;
+int find = 17;
+
+int index = 0;
+
+while (index<n);
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
